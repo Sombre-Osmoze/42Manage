75f9e24d4293fee579a2bdc3b80b47bcda5b8b35
Marcus Florentin <fmarcus28@gmx.fr>
2018-08-14 22:17:13 +0200

    Initial Commit

---
+++ 42ManageTests/_2ManageTests.swift
@@ -0,0 +1,34 @@
+//
+//  _2ManageTests.swift
+//  42ManageTests
+//
+//  Created by Marcus Florentin on 14/08/2018.
+//  Copyright © 2018 Marcus Florentin. All rights reserved.
+//
+
+import XCTest
+@testable import _2Manage
+
+class _2ManageTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
