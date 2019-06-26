333433264a76349a5e8e865f3e1a705fe5ae667c
Marcus Florentin <fmarcus28@gmx.fr>
2019-06-26 15:00:29 +0200

    Cleaning AW project

---
+++ Manage42/APIControls.swift
@@ -0,0 +1,13 @@
+//
+//  APIControls.swift
+//  Manage42
+//
+//  Created by Marcus Florentin on 12/06/2019.
+//  Copyright © 2019 Marcus Florentin. All rights reserved.
+//
+
+import Foundation
+import API42
+import UIKit
+
+
